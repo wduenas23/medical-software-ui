affa5e4c8bb0beddb36482c318492e0aa83e48a5
wduenas23 <waltdusan@gmail.com>
2021-10-25 23:50:20 -0600

    Adding charts

--- src/app/control-ingresos/ci-routing.module.ts
+++ src/app/control-ingresos/ci-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { IngresosComponent } from './pages/ingresos/ingresos.component';
+import { ReporteComponent } from './pages/reporte/reporte.component';
 
 
 const routes: Routes = [
@@ -12,6 +13,10 @@ const routes: Routes = [
       {
         path: 'ingresos',
         component: IngresosComponent
+      },
+      {
+        path: 'reporte',
+        component: ReporteComponent
       },
       {
         path: '**',
--- src/app/control-ingresos/pages/reporte/reporte.component.ts
+++ src/app/control-ingresos/pages/reporte/reporte.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions,ChartType } from 'chart.js';
+import {  Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-reporte',
@@ -8,6 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReporteComponent implements OnInit {
 
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public barChartLabels: Label[] = ['MESOTERAPIA REDUCTORA GRASA ABDOMINAL', 'CARBOXITERAPIA CORPORAL / 10 SESIONES', 'MESOTERAPIA REDUCTORA GRASA ABDOMINAL', 'RINOMODELACION PERMANENTE CON HILOS', 'CARBOXITERAPIA (OJERAS, ROSTRO, PAPADA Y CUELLO)', 'PLASMA RICO EN PLAQUETAS FACIAL CON DERMAPEN', 'PLASMA RICO EN PLAQUETAS (CAIDA CABELLO)', 'BOTOX 1 AREA', 'VIGOTE', 'ESPALDA', 'CONSULTA MEDICA', 'HIDRAFACIAL (LIMPIEZA FACIAL PREMIUM)'];
+  public barChartType: ChartType = 'horizontalBar';
+  public barChartLegend = false;
+  public barChartPlugins = [];
+
+  public barChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40,70,89,100,256,12], label: 'Consultas' }   
+  ];
   constructor() { }
 
   ngOnInit(): void {
